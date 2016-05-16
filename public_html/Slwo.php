726b5158c3cfef84c8baf3ba6f9e491f3d2395e3
root <root@ns.local.domain>
2016-05-16 15:42:42 -0400

    Add the word min. chars feature.

--- public_html/Slwo.php
+++ public_html/Slwo.php
@@ -9,6 +9,7 @@ namespace Deirde\Slwo {
     class Slwo {
 
         private $contents;
+        private $min = 4;
         private $sort;
 
         /**
@@ -41,8 +42,18 @@ namespace Deirde\Slwo {
             if (in_array($sort, $_)) {
                 $this->sort = $sort;
             } else {
-                trigger_error('The sort parameters must be one of the following: ' . implode($_, ', ') .
+                trigger_error('The sort parameter must be one of the following: ' . implode($_, ', ') .
                     '. Ref. http://php.net/manual/en/array.sorting.php', E_USER_ERROR);
+            }
+
+        }
+
+        public function min($min) {
+
+            if (!is_integer($min)) {
+                trigger_error('The min parameter must be an integer', E_USER_ERROR);
+            } else {
+                $this->min = $min;
             }
 
         }
@@ -59,7 +70,8 @@ namespace Deirde\Slwo {
             /**
              * Get the words.
              */
-            preg_match_all('/([a-zA-Z]|\xC3[\x80-\x96\x98-\xB6\xB8-\xBF]|\xC5[\x92\x93\xA0\xA1\xB8\xBD\xBE]){4,}/', $contents, $occurrences);
+            preg_match_all('/([a-zA-Z]|\xC3[\x80-\x96\x98-\xB6\xB8-\xBF]|\xC5[\x92\x93\xA0\xA1\xB8\xBD\xBE]){' . $this->min . ',}/',
+                $contents, $occurrences);
 
             /**
              * Lowercase.
--- public_html/index.php
+++ public_html/index.php
@@ -5,6 +5,7 @@ include 'Slwo.php';
 $contents = file_get_contents('https://en.wikipedia.org/wiki/Hello,_Dolly!_(musical)');
 
 $Slwo = new Deirde\Slwo\Slwo($contents);
+$Slwo->min(1);
 $Slwo->sort('ksort');
 $Slwo->htmlOutput();
 
